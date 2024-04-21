0a56b883eacd456f5f8bfc37e44e3ce5d430d227
13574798@qq.com <13574798@qq.com>
2024-04-22 06:37:57 +0800

    parsing args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::env;
 fn main() {
-    println!("Hello, world!");
+    
+    let args:Vec<String>  =env::args().collect();
+    
+    let search_string   = &args[1];
+    let file_name = &args[2];
+    println!("filename is:{0},search string is:{1}",file_name,search_string);
 }
