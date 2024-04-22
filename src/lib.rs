12caa255bbca8433b6e6c3e9acda99ddb088665b
13574798@qq.com <13574798@qq.com>
2024-04-22 09:28:03 +0800

    v0.0.4

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,43 @@
 
 use std::fs;
 use std::error::Error;
+#[cfg(test)]
 
+mod tests{
+    use super::*;
+
+    #[test]
+    fn for_one_result(){
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+
+
+
+}
+
+pub fn search<'a>(query:&str,contents:&'a str)->Vec<&'a str>{
+    let mut result:Vec<&str> = Vec::new();
+    for line in contents.lines(){
+        if line.contains(query){
+            result.push(line);
+        }
+    }
+    result
+}
 pub fn run(config:Config)->Result<(),Box<dyn Error>>{
 
-    let content  = fs::read_to_string(config.filename);
-
+    let content  = fs::read_to_string(config.filename).unwrap();
+    
+     for line in search(&config.str, content.as_str()){
+        println!("{line}")
+     }
         Ok(())
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use std::env;
-use std::error;
-use std::fs;
 use std::process;
-use std::error::Error;
+
 use minigrep::Config;
 use minigrep::run;
 fn main() {
@@ -15,7 +13,7 @@ fn main() {
     });
 
     if let Err(error) = run(config){
-
+        print!("system occur error:{}",error);
     }
 
 }
