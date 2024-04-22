a90a39f63ba5c684d6df6824ef47759ea3e85231
13574798@qq.com <13574798@qq.com>
2024-04-22 09:36:07 +0800

    fmt codes

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,12 @@
-
-use std::fs;
 use std::error::Error;
+use std::fs;
 #[cfg(test)]
 
-mod tests{
+mod tests {
     use super::*;
 
     #[test]
-    fn for_one_result(){
+    fn for_one_result() {
         let query = "duct";
         let contents = "\
 Rust:
@@ -16,30 +15,24 @@ Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
-
-
-
-
 }
 
-pub fn search<'a>(query:&str,contents:&'a str)->Vec<&'a str>{
-    let mut result:Vec<&str> = Vec::new();
-    for line in contents.lines(){
-        if line.contains(query){
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result: Vec<&str> = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
             result.push(line);
         }
     }
     result
 }
-pub fn run(config:Config)->Result<(),Box<dyn Error>>{
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let content = fs::read_to_string(config.filename).unwrap();
 
-    let content  = fs::read_to_string(config.filename).unwrap();
-    
-     for line in search(&config.str, content.as_str()){
+    for line in search(&config.str, content.as_str()) {
         println!("{line}")
-     }
-        Ok(())
-
+    }
+    Ok(())
 }
 pub struct Config<'a> {
     str: &'a String,
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
 use std::env;
 use std::process;
 
-use minigrep::Config;
 use minigrep::run;
+use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     //exception process,using unwrap_or_else
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    if let Err(error) = run(config){
-        print!("system occur error:{}",error);
+    if let Err(error) = run(config) {
+        eprint!("system occur error:{}", error);
     }
-
 }
-
