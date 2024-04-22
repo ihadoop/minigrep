c8a5dec5277503a6e88c17e1a5e107b08e67e390
13574798@qq.com <13574798@qq.com>
2024-04-22 08:28:21 +0800

    v0.0.2

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,27 @@
 use std::env;
 use std::fs;
 fn main() {
-    
-    let args:Vec<String>  =env::args().collect();
-    
-   let config =parse_config(&args);
-   println!("{}",config.filename)
+    let args: Vec<String> = env::args().collect();
 
-}
-fn parse_config(args:&[String])->Config{
-    //check length
-    if args.len()<2 {
-      panic!("");
-    }
-    
-    Config{
-        str:&args[1],
-        filename:&args[2]
-    }
-
+    let config = Config::new(&args);
+    println!("{}", config.filename)
 }
 
-struct Config<'a>{
-    str:&'a String,
-    filename:&'a String
+struct Config<'a> {
+    str: &'a String,
+    filename: &'a String,
+}
+
+impl Config<'_> {
+    fn new(args: &[String]) -> Config {
+        //check length
+        if args.len() < 2 {
+            panic!("");
+        }
+
+        Config {
+            str: &args[1],
+            filename: &args[2],
+        }
+    }
 }
